c4e28e07cddb72161526ce590b15aba3ae250139
Danditfq <75623056+Danditfq@users.noreply.github.com>
2022-10-31 21:05:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {
     production : {
-        host :"database-dandi.c2kfdi3bqt2u.ap-southeast-2.rds.amazonaws.com",
+        host :"dandii-db.c2kfdi3bqt2u.ap-southeast-2.rds.amazonaws.com",
         port : "3306",
         username :"dandi",
         password :"dandi123",
